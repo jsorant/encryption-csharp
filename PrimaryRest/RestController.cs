1517759708ebea1fe18a29e703b824317610bb65
Jérémy Sorant <jsorant@gmail.com>
2025-04-06 10:34:19 +0200

    Playing with Rest Controllers

---
+++ PrimaryRest/RestController.cs
@@ -0,0 +1,19 @@
+﻿
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PrimaryRest;
+
+public class RestController : Controller
+{
+    public async Task<IActionResult> Encrypt(EncryptModel encryptModel)
+    {
+        var cipher = new CaesarCipher(encryptModel.Shift);
+        var encryptedText = cipher.Encrypt(encryptModel.PlainText);
+        return View(new EncryptedModel(encryptedText));
+    }
+}
+
+public record EncryptModel(string PlainText, int Shift);
+
+public record EncryptedModel(string EncryptedText);
---
+++ PrimaryRestTest/RestControllerTest.cs
@@ -0,0 +1,25 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using PrimaryRest;
+
+namespace PrimaryRestTest;
+
+public class Tests
+{
+    [Test]
+    public async Task TestEncryptRoute()
+    {
+        var controller = new RestController();
+
+        var encryptModel = new EncryptModel("ABC", 2);
+        
+        var result = await controller.Encrypt(encryptModel);
+        
+        var viewResult = result as ViewResult;
+        Assert.That(viewResult, Is.Not.Null);
+        
+        var model = viewResult.ViewData.Model as EncryptedModel;
+        Assert.That(model, Is.Not.Null);
+        Assert.That(model.EncryptedText, Is.EqualTo("CDE"));
+    }
+}
